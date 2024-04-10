5c96a8669b727ee57eff3505758c87baf1d404b4
Georg Graf (SPG) <grafg@spengergasse.at>
2024-04-10 12:00:07 +0200

    stunde 10.4.

--- Abgabe_10.4_plf/query.js
+++ Abgabe_10.4_plf/query.js
@@ -1,45 +1,53 @@
-const { PrismaClient } = require("@prisma/client")
-const prisma = new PrismaClient();
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient({ log: ['query'] });
 
 async function getAllZoos() {
-  return await prisma.zoo.findMany();
+    return await prisma.zoo.findMany();
 }
 
 async function getRandomZoo() {
-  const zoos = await getAllZoos();
-  return zoos[Math.floor(Math.random() * zoos.length)];
+    const zoos = await getAllZoos();
+    return zoos[Math.floor(Math.random() * zoos.length)];
 }
 
 async function getAbteilungenForZoo(zooId) {
-  return await prisma.Abteilung.findMany({ where: { ZooId: zooId } });
+    return await prisma.Abteilung.findMany({ where: { ZooId: zooId } });
 }
 
 async function getAbteilungenAndTiereCountForZoo(zooId) {
-  const abteilungen = await prisma.Abteilung.findMany({ 
-    where: { ZooId: zooId },
-    include: { MTiere: true }
-  });
+    const abteilungen = await prisma.Abteilung.findMany({
+        where: { ZooId: zooId },
+        include: { _count: { select: { MTiere: true } } },
+    });
 
-  return abteilungen.map(abteilung => ({
-    abteilungId: abteilung.cuid,
-    abteilungName: abteilung.Name,
-    animalCount: abteilung.MTiere.length,
-  }));
+    return abteilungen.map((abteilung) => ({
+        abteilungId: abteilung.id,
+        abteilungName: abteilung.Name,
+        animalCount: abteilung.MTiere.length,
+    }));
 }
 
 async function getMitarbeiterInZoo(zooId) {
-  return await prisma.mitarbeiter.findMany({
-    where: { Abteilungen: { some: { ZooId: zooId } } },
-    include: { Abteilungen: true }
-  });
+    return await prisma.mitarbeiter.findMany({
+        where: { Abteilungen: { some: { ZooId: zooId } } },
+        include: { Abteilungen: true },
+    });
 }
 
+async function main() {
+    console.log(JSON.stringify(await getAllZoos(), null, 2));
 
-getAllZoos().then(console.log);
+    randomZoo = await getRandomZoo();
+    console.log('Random Zoo:', randomZoo);
+    getAbteilungenForZoo(randomZoo.id).then((o) =>
+        console.log(JSON.stringify(o, null, 2))
+    );
+    getAbteilungenAndTiereCountForZoo(randomZoo.id).then((o) =>
+        console.log(JSON.stringify(o, null, 2))
+    );
+    getMitarbeiterInZoo(randomZoo.id).then((o) =>
+        console.log(JSON.stringify(o, null, 2))
+    );
+}
 
-getRandomZoo().then(randomZoo => {
-  console.log('Random Zoo:', randomZoo);
-  getAbteilungenForZoo(randomZoo.cuid).then(console.log);
-  getAbteilungenAndTiereCountForZoo(randomZoo.cuid).then(console.log);
-    getMitarbeiterInZoo(randomZoo.cuid).then(console.log);
-});
+main().then(() => prisma.$disconnect());
--- Abgabe_10.4_plf/seed.js
+++ Abgabe_10.4_plf/seed.js
@@ -1,114 +1,107 @@
-const {PrismaClient} = require("@prisma/client")
-const {faker} = require("@faker-js/faker");
-
+const { PrismaClient } = require('@prisma/client');
+const { faker } = require('@faker-js/faker');
 
 const prisma = new PrismaClient();
 
-class FakeZoo{
-    constructor(){
+class FakeZoo {
+    constructor() {
         this.Land = faker.location.country();
         this.Stadt = faker.location.city();
         this.Adresse = faker.location.streetAddress();
         this.baujahr = faker.date.past();
-
-
     }
 }
 
-class FakeAbteilung{
-    constructor(zooid){
+class FakeAbteilung {
+    constructor(zooid) {
         this.Name = faker.animal.type();
         this.ZooId = zooid;
-
     }
-
 }
 
-class FakeTier{
-    constructor(_abteilungsId){
+class FakeTier {
+    constructor(_abteilungsId) {
         this.Art = faker.animal.type();
         this.Name = faker.person.firstName();
         this.AbteilungsId = _abteilungsId;
     }
 }
 
-class FakeMitarbeiter{
-    constructor(){
+class FakeMitarbeiter {
+    constructor() {
         this.name = faker.person.fullName();
-
     }
 }
 
-
 async function main() {
-    
-
-    for (let i = 0;i<5;i++){
-        const _fakeZoo = new FakeZoo;
-        const Zoo = await prisma.Zoo.create(
-            {data: _fakeZoo}
-        )
+    for (let i = 0; i < 5; i++) {
+        const _fakeZoo = new FakeZoo();
+        const Zoo = await prisma.Zoo.create({ data: _fakeZoo });
     }
 
-  const ZooIds = (await prisma.Zoo.findMany({ select: { cuid: true } })).map(_ => _.cuid);
-const zooAbteilungen = {};
+    const ZooIds = (await prisma.Zoo.findMany({ select: { id: true } })).map(
+        (_) => _.id
+    );
+    const zooAbteilungen = {};
 
-for (let zooId of ZooIds) {
-  const abteilungenCount = faker.number.int({ min: 2, max: 7 });
-  zooAbteilungen[zooId] = new Set();
+    for (let zooId of ZooIds) {
+        const abteilungenCount = faker.number.int({ min: 2, max: 7 });
+        zooAbteilungen[zooId] = new Set();
 
-  for (let i = 0; i < abteilungenCount; i++) {
-    let abteilungName;
-    do {
-      abteilungName = faker.animal.type();
-    } while (zooAbteilungen[zooId].has(abteilungName));
+        for (let i = 0; i < abteilungenCount; i++) {
+            let abteilungName;
+            do {
+                abteilungName = faker.animal.type();
+            } while (zooAbteilungen[zooId].has(abteilungName));
 
-    zooAbteilungen[zooId].add(abteilungName);
+            zooAbteilungen[zooId].add(abteilungName);
 
-    const _fakeAbteilung = new FakeAbteilung(zooId);
-    _fakeAbteilung.Name = abteilungName;
+            const _fakeAbteilung = new FakeAbteilung(zooId);
+            _fakeAbteilung.Name = abteilungName;
 
-    const Abteilung = await prisma.Abteilung.create({
-      data: _fakeAbteilung,
-    });
-  }
-    }
-    
-    const AbteilungsIds = (await prisma.Abteilung.findMany({ select: { cuid: true } })).map(_ => _.cuid);
-    console.log(...AbteilungsIds);
-    for (let j = 0; j<AbteilungsIds.length-1;j++){
-        for (let i =0; i<faker.number.int({min: 5, max: 20}); i++)
-        {
-            const _fakeTier = new FakeTier(AbteilungsIds[j]);
-            const Tier = await prisma.Tier.create(
-            { data: {..._fakeTier},
-            
-            }
-            )
+            const Abteilung = await prisma.Abteilung.create({
+                data: _fakeAbteilung,
+            });
         }
     }
 
-    for (let i=0; i<100;i++){
-        const _fakeMitarbeiter = new FakeMitarbeiter();
-        const Mitarbeiter = await prisma.mitarbeiter.create(
-            {data: {..._fakeMitarbeiter}}
-        )
+    const AbteilungsIds = (
+        await prisma.Abteilung.findMany({ select: { id: true } })
+    ).map((_) => _.id);
+    console.log(...AbteilungsIds);
+    for (let j = 0; j < AbteilungsIds.length - 1; j++) {
+        for (let i = 0; i < faker.number.int({ min: 5, max: 20 }); i++) {
+            const _fakeTier = new FakeTier(AbteilungsIds[j]);
+            const Tier = await prisma.Tier.create({ data: { ..._fakeTier } });
+        }
     }
-    const MitarbeiterIds = (await prisma.mitarbeiter.findMany( {select: {cuid: true}})).map(_ => _.cuid);
-        
+
+    for (let i = 0; i < 100; i++) {
+        const _fakeMitarbeiter = new FakeMitarbeiter();
+        const Mitarbeiter = await prisma.mitarbeiter.create({
+            data: { ..._fakeMitarbeiter },
+        });
+    }
+    const MitarbeiterIds = (
+        await prisma.mitarbeiter.findMany({ select: { id: true } })
+    ).map((_) => _.id);
+
     for (let mitarbeiterId of MitarbeiterIds) {
-    const abteilungenCount = faker.number.int({ min: 1, max: 4 });
+        const abteilungenCount = faker.number.int({ min: 1, max: 4 });
 
         for (let i = 0; i < abteilungenCount; i++) {
-            const abteilungId = AbteilungsIds[faker.number.int({ min: 0, max: AbteilungsIds.length - 1 })];
+            const abteilungId =
+                AbteilungsIds[
+                    faker.number.int({ min: 0, max: AbteilungsIds.length - 1 })
+                ];
 
             await prisma.mitarbeiter.update({
-            where: { cuid: mitarbeiterId },
-            data: {
-                Abteilungen: {
-                connect: { cuid: abteilungId },
+                where: { id: mitarbeiterId },
+                data: {
+                    Abteilungen: {
+                        connect: { id: abteilungId },
+                    },
                 },
-            },
             });
         }
     }
